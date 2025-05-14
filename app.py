de4b89dd429db6a0d4c7935b8025153d4511c7f0
Arshdeep Singh <67805467+botenstein@users.noreply.github.com>
2025-05-14 18:43:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -129,4 +129,4 @@ def detect():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
